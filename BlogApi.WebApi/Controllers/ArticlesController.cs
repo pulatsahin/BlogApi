f78b6bac8cb81e4b9d6031690a1d943824a066a4
pulatsahin <pulatsahin@hotmail.com>
2020-01-12 22:25:29 +0300

    Create ArticlesController.cs

---
+++ BlogApi.WebApi/Controllers/ArticlesController.cs
@@ -0,0 +1,49 @@
+﻿using BlogApi.WebApi.DataAccess.Abstract;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApi.WebApi.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class ArticlesController : Controller
+    {
+        private IArticleDal _articleDal;
+
+        public ArticlesController(IArticleDal articleDal)
+        {
+            _articleDal = articleDal;
+           
+        }
+
+        [HttpGet]
+        [HttpPost("authenticate")]
+        public IActionResult Get()
+        {
+
+            try
+            {
+                var articles = _articleDal.GetList();
+
+                if (articles == null)
+                {
+                   
+                    return NotFound("There is no article");
+                }
+
+                return Ok(articles);
+
+
+            }
+            catch { }
+
+
+            return BadRequest();
+
+        }
+
+
+
+    }
+}
