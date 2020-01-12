64b6c06c4a19a212e5959511fd515be0ad8b41ec
pulatsahin <pulatsahin@hotmail.com>
2020-01-12 22:15:23 +0300

    Create ApplicationUser.cs

---
+++ BlogApi.DataAccess/Concreate/EntityFramework/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlogApi.DataAccess.Concreate.EntityFramework
+{
+    public class ApplicationUser: IdentityUser
+    {
+    }
+}
