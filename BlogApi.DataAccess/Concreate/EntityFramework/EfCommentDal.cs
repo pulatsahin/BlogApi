16f7131e516eabba104dcacaf2b00f448beeb0b6
pulatsahin <pulatsahin@hotmail.com>
2020-01-12 22:15:20 +0300

    Create EfCommentDal.cs

---
+++ BlogApi.DataAccess/Concreate/EntityFramework/EfCommentDal.cs
@@ -0,0 +1,10 @@
+﻿using BlogApi.Core.DataAccess.EntityFramework;
+using BlogApi.Core.Entities;
+using BlogApi.DataAccess.Abstract;
+
+namespace BlogApi.DataAccess.Concreate.EntityFramework
+{
+    public class EfCommentDal : EfEntityRepositoryBase<Comment, WebApiBlogContext>, ICommentDal
+    {
+    }
+}
