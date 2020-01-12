81fb1ff17b4e19fff2367b452ae2eed44b4154b9
pulatsahin <pulatsahin@hotmail.com>
2020-01-12 22:14:27 +0300

    Create IArticleDal.cs

---
+++ BlogApi.DataAccess/Abstract/IArticleDal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BlogApi.Core.DataAccess;
+using BlogApi.Core.Entities;
+
+namespace BlogApi.DataAccess.Abstract
+{
+    public interface IArticleDal : IEntityRepository<Article>
+    {
+        List<ArticleModel> GetArticlesWithDetails();
+    }
+}
