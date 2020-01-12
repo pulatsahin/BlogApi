4da038ccb8361131bcd7381da185b1b7c387cde5
pulatsahin <pulatsahin@hotmail.com>
2020-01-12 22:26:50 +0300

    Create Comment.cs

---
+++ BlogApi.WebApi/Entities/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlogApi.WebApi.Entities
+{
+    public class Comment:IEntity
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+        public int ArticleID { get; set; }
+
+        public string UserName { get; set; }
+        public string CommentDetail { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime CreateDate { get; set; }
+
+    }
+}
