1d560604fd0c2df6b398ac6cf5568466657537a3
pulatsahin <pulatsahin@hotmail.com>
2020-01-12 22:25:38 +0300

    Create IEntityRepository.cs

---
+++ BlogApi.WebApi/DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using BlogApi.WebApi.Entities;
+
+namespace BlogApi.WebApi.Core.DataAccess
+{
+    public interface IEntityRepository<T> 
+        where T : class, IEntity, new()
+        
+    {
+        T Get(Expression<Func<T, bool>> filter = null);
+        List<T> GetList(Expression<Func<T, bool>> filter = null);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+    }
+}
