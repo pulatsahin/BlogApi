000ae315610c93f2fd65540d5df26b796f8f1593
pulatsahin <pulatsahin@hotmail.com>
2020-01-12 22:13:30 +0300

    Create Article.cs

---
+++ BlogApi.Core/Entities/Article.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlogApi.Core.Entities
+{
+    public class Article:IEntity
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+        public  string ArticleName { get; set; }
+
+        public string ArticleDetail { get; set; }
+        
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public  DateTime CreateDate { get; set; }
+
+    }
+}
