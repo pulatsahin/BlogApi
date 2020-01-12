ea78b0bad21da251d343f4f41207ad6142eb92a8
pulatsahin <pulatsahin@hotmail.com>
2020-01-12 22:16:14 +0300

    Create EfArticleDal.cs

---
+++ BlogApi.DataAccess/Concreate/EntityFramework/EfArticleDal.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using BlogApi.Core.DataAccess.EntityFramework;
+using BlogApi.Core.Entities;
+using BlogApi.DataAccess.Abstract;
+
+
+namespace BlogApi.DataAccess.Concreate.EntityFramework
+{
+
+
+    public class EfArticleDal : EfEntityRepositoryBase<Article, WebApiBlogContext >, IArticleDal
+    {
+        public List<ArticleModel> GetArticlesWithDetails()
+        {
+
+            using (WebApiBlogContext context = new WebApiBlogContext())
+            {
+                var result = from a in context.Articles
+                  
+                    select new ArticleModel
+                    {
+                        id = a.id, 
+                        ArticleDetail = a.ArticleDetail,
+                        ArticleName = a.ArticleName, 
+                        CreateDate = a.CreateDate
+                       
+                    };
+                return result.ToList();
+            }
+        }
+    }
+}
