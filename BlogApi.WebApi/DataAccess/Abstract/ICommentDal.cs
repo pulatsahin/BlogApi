f2fe3c857bacc465922b905973fade017f1c7a75
pulatsahin <pulatsahin@hotmail.com>
2020-01-12 22:26:13 +0300

    Create ICommentDal.cs

---
+++ BlogApi.WebApi/DataAccess/Abstract/ICommentDal.cs
@@ -0,0 +1,10 @@
+﻿using BlogApi.WebApi.Core.DataAccess;
+using BlogApi.WebApi.Entities;
+
+
+namespace BlogApi.WebApi.DataAccess.Abstract
+{
+    public interface ICommentDal:IEntityRepository<Comment>
+    {
+    }
+}
